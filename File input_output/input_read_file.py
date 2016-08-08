011b0ae99fb52b3fee53bffaed37f911c74c5fc0
Victor Allen <vamuigua@gmail.com>
2016-08-08 16:33:52 +0300

    modified

--- File input_output/input_read_file.py
+++ File input_output/input_read_file.py
@@ -11,7 +11,6 @@ my_file.close()
 
 #Open the .txt file
 my_file = open("details.txt","r")
-
 #Read the content line by line while printing it out
 print my_file.readline()
 print my_file.readline()
@@ -19,3 +18,25 @@ print my_file.readline()
 
 #Close the .txt file
 my_file.close()
+
+#Reading only the first 10 characters(Including the spaces)
+my_file = open("details.txt","r+")
+reading = my_file.read(10)
+print "Read string is: ",reading
+
+#Read the Current File Position
+position = my_file.tell()
+print "The current position is: ",position
+
+#Reading the next 9 characters
+reading2 = my_file.read(5)
+print "Read string is: ",reading2
+#Reading the current file position after reading the next 9 characters
+position = my_file.tell()
+print "The current position is: ",position
+
+#Close the File
+my_file.close()
+
+
+
